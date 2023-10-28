ea71673dd7f4d8537f13e3d5a01ca3f5b20c57ca
Daniel Long <danielwlong618@gmail.com>
2023-10-27 21:27:49 -0500

    added title id

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let title = document.createElement('h1')
     title.innerText = 'Welcome to Danny\'s Margarita Bar'
+    title.id = 'title'
     document.body.appendChild(title)
-    
-
 
 })
